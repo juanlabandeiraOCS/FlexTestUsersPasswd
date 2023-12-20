fbcee78b7dd48f1f634828e5d39fd2e8efe3818a
Juan Labandeira <juan.labandeira@olympicchannel.com>
2023-12-20 17:23:29 +0000

    Commit 20231220-01

---
+++ listusers-FlexSTG.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://stgflex.mam.olympicchannel.com/api/users"
+
+payload = {}
+headers = {
+  'Authorization': 'Basic YXJlbWE6aFM0M0BEdlR5Q0t2'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
